50bf7cbdf75976ad572994837e7f54835fe442a5
ejaay-tester <ejaaay.tester@gmail.com>
2025-09-04 23:30:38 +0800

    Third Commit Add the workflow_dispatch on the YAML file Update the test details Separate the example.spec.ts to a new folder

--- tests/practice-login.spec.ts
+++ tests/practice-login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test"
 
-test("[PROJ-T1] should load the homepage successfully", async ({ page }) => {
+test("Should load the homepage successfully", async ({ page }) => {
   await page.pause()
   await page.goto("https://playwright.dev/")
   await expect(page).toHaveTitle(/Playwright/)
