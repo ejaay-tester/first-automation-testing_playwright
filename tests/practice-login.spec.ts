80387c64ce1a3230c713c01b96558488c3f184c0
ejaay-tester <ejaaay.tester@gmail.com>
2025-09-05 00:01:12 +0800

    Fourth Commit Create new test script for successful login

--- tests/practice-login.spec.ts
+++ tests/practice-login.spec.ts
@@ -1,7 +1,16 @@
 import { test, expect } from "@playwright/test"
 
 test("Should load the homepage successfully", async ({ page }) => {
-  await page.pause()
   await page.goto("https://playwright.dev/")
   await expect(page).toHaveTitle(/Playwright/)
+})
+
+test("Verify that a user can successfully log in with valid credentials", async ({
+  page,
+}) => {
+  await page.goto("https:saucedemo.com")
+  await page.getByPlaceholder("Username").fill("standard_user")
+  await page.getByPlaceholder("Password").fill("secret_sauce")
+  await page.getByRole("button", { name: "Login" }).click()
+  await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html")
 })
